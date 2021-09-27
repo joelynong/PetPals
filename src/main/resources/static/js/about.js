9ac34ad89a4ca8749b049c587718054c34df249a
Joelyn <joelynong@yahoo.com>
2021-09-27 10:53:44 +0800

    add about html css images

--- src/main/resources/static/js/about.js
+++ src/main/resources/static/js/about.js
@@ -1,3 +1,22 @@
+ // When the user clicks on the button, scroll to the top of the document
+
+ mybutton = document.getElementById("myBtn1");
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+
 // When the user scrolls down 20px from the top of the document, show the button
 
 window.onscroll = function () {
@@ -19,10 +38,11 @@ document.querySelectorAll(".story-btn").forEach((btn) => {
   });
 });
 
-// When the user clicks on the button, scroll to the top of the document
 
-function topFunction() {
-  document.body.scrollTop = 0;
 
-  document.documentElement.scrollTop = 0;
-}
+//function topFunction() {
+//  document.body.scrollTop = 0;
+//
+//  document.documentElement.scrollTop = 0;
+//}
+
